05d8f1038941bd49eef05f3c7bb66c1e4ef3f345
Robby <robby23si@pcr.ac.id>
2025-09-22 08:59:08 +0700

    penambahan halaman home

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
@@ -29,3 +30,6 @@ route::get('/mahasiswa/{param1?}', [MahasiswaController::class, 'show']);
 Route::get('/about', function () {
     return view('halaman-about');
 });
+
+Route::get('/home', [HomeController::class, 'index']);
+
